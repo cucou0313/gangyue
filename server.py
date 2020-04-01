d2a3ce3f01658a61f83bc01da0a4f57713418ddf
unknown <you@example.com>
2020-04-01 22:58:59 +0800

    MySQL+登录注册

--- server.py
+++ server.py
@@ -1,6 +1,10 @@
 from flask import Flask, redirect, url_for, render_template, request, make_response
 # url_for()函数对于动态构建特定函数的URL非常有用。该函数接受函数的名称作为第一个参数，以及一个或多个关键字参数，每个参数对应于URL的变量部分。
+import pymysql
 app = Flask(__name__)
+db = pymysql.connect("localhost", "root", "0313", "gangyue")
+db.ping(reconnect=True)
+cursor = db.cursor()
 
 
 @app.route('/')
@@ -8,32 +12,61 @@ def welcome():
     return render_template('welcome.html')
 
 
-# 登录方法，连接sqlite进行账号验证
+# 登录方法，连接MySQL进行账号验证
 @app.route('/sign_in', methods=['POST', 'GET'])
 def sign_in():
     if request.method == 'POST':
-        name = request.form['userName']
-        password = request.form['userPsd']
-        if name == 'admin' and password == 'admin':
-            # 验证通过，传参并跳转至主页面
-            # return redirect(url_for('main', userName=request.form['userName']))
-            resp = make_response(redirect(url_for('main')))
-            resp.set_cookie('userName', name)
-            resp.set_cookie('userPsd', password)
-            return resp
-            # return redirect(url_for('main'))
-        else:
-            # 登录失败调回初始登录页
-            return redirect(url_for('sign_in'))
+        userId = request.form['userId']
+        userPsd = request.form['userPsd']
+        # if userId is not None and userPsd is not None:
+        sql = "SELECT * FROM user WHERE student_id = %s and password = %s" % (
+            userId, userPsd)
+        try:
+            cursor.execute(sql)
+            # fetchone为空时的返回集为None
+            res = cursor.fetchone()
+            if res is None:
+                print('User info error')
+                # 登录失败调回初始登录页
+                return redirect(url_for('sign_in'))
+            else:
+                # 验证通过，传参并跳转至主页面
+                resp = make_response(redirect(url_for('main')))
+                resp.set_cookie('userId', userId)
+                resp.set_cookie('userPsd', userPsd)
+                return resp
+        except Exception as e:
+            print('error', e)
+        finally:
+            pass
     elif request.method == 'GET':
         return render_template('sign_in.html')
 
 
-# 注册方法，连接sqlite写入
+# 注册方法，连接MySQL写入
 @app.route('/sign_up', methods=['POST', 'GET'])
 def sign_up():
-    if request.method == 'POST' and request.form[
-            'userName'] == 'admin' and request.form['userPsd'] == 'admin':
+    userId = request.form['userId']
+    userPsd = request.form['userPsd']
+    if request.method == 'POST':
+        userIdvalid = True
+        sql = "SELECT * FROM user WHERE student_id = %s" % (userId)
+        try:
+            cursor.execute(sql)
+            # fetchone为空时的返回集为None
+            res = cursor.fetchone()
+            if res is None:
+                userIdvalid = False
+            else:
+                print('exist')
+        except Exception as e:
+            print('error', e)
+        finally:
+            if not userIdvalid:
+                sql = "INSERT INTO user(student_id,password) VALUES ('%s', '%s')" % (
+                    userId, userPsd)
+                cursor.execute(sql)
+                db.commit()
         # 注册成功，跳转至主页面
         return redirect(url_for('sign_in'))
     elif request.method == 'GET':
@@ -42,12 +75,12 @@ def sign_up():
 
 @app.route('/main')
 def main():
-    userName = request.cookies.get('userName')
+    userId = request.cookies.get('userId')
     allText = [[1, "10:00", "运动", "text"], [2, "10:00", "运动", "text"],
                [3, "10:00", "运动", "text"], [4, "10:00", "运动", "text"],
                [5, "10:00", "运动", "text"], [6, "10:00", "运动", "text"]]
     # return 'Welcome %s ,logged in successfully' % userName
-    return render_template('main.html', userName=userName, allText=allText)
+    return render_template('main.html', userId=userId, allText=allText)
 
 
 @app.route('/user_info')
